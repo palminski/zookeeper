a2441aacc1171fb149585476f7c4274a03494991
palminski <williambolls98@gmail.com>
2022-09-14 13:11:04 -0700

    Added front end conectivity and ability to add and view animals

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+//Allow the css and js file to be found
+app.use(express.static('public'));
 
 const { animals } = require('./data/animals');
 
@@ -119,7 +121,25 @@ app.post('/api/animals', (req,res) => {
     
 });
 
+//Serving up HTML
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
 
+app.get('/animals', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req,res) => {  //Wildcard must go last or everything will go tothis page
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+//<><><><><><><>
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
